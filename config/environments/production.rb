bdef726fba87cf6042911c9f38a3cbdfd5355bb7
danielbarrios76 <danielbarrios76@hotmail.com>
2018-01-31 21:31:28 +0000

    Finish fixed errors SMTP configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,34 +62,34 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   
   # HEROKU configuration SMTP SENGRID
-  #config.action_mailer.perform_caching = false
-  #config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.delivery_method = :smtp
-  #host = 'glacial-hamlet-38204.herokuapp.com'
-  #config.action_mailer.default_url_options = { host: host }
-  #ActionMailer::Base.smtp_settings = {
-  #  :address        => 'smtp.sendgrid.net',
-  #  :port           => '587',
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => 'heroku.com',
-  #  :enable_starttls_auto => true
-  #}
-
-  #GMAIL configuration SMTP
-  host = 'gmail.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    user_name: 'danielbarrios76@gmail.com',
-    password: 'Cl4v3G00gl3001#g',
-    enable_starttls_auto: true
+  host = 'glacial-hamlet-38204.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
+
+  #GMAIL configuration SMTP
+  #host = 'gmail.com'
+  #config.action_mailer.default_url_options = { host: host }
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  authentication: "plain",
+  #  user_name: ENV['GMAIL_USERNAME'],
+  #  password: ENV['GMAIL_PASSWORD'],
+  #  enable_starttls_auto: true
+  #}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
