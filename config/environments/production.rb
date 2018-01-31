e43daa7cb71c9ce57854360b038f5c3eb782b8b3
danielbarrios76 <danielbarrios76@hotmail.com>
2018-01-31 20:51:24 +0000

    fixed errors in SMTP configuration y 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   #}
 
   #GMAIL configuration SMTP
+  host = 'gmail.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
